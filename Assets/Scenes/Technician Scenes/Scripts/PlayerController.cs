464708e5a398711d76b5690f75f3aff378d12364
Marnige <dragonmasteroffire@gmail.com>
2025-05-29 01:36:36 +0800

    Animation Added for Movement

--- Assets/Scenes/Technician Scenes/Scripts/PlayerController.cs
+++ Assets/Scenes/Technician Scenes/Scripts/PlayerController.cs
@@ -8,16 +8,34 @@ public class PlayerController : MonoBehaviour
     public bool isMoving;
     public Vector2 input;
 
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>(); //Start the animation upon awake
+    }
+
 
     private void Update()
     {
         if (!isMoving)
         {
-            input.x = Input.GetAxisRaw("Horizontal");
+            input.x = Input.GetAxisRaw("Horizontal"); // Get keyboard input
             input.y = Input.GetAxisRaw("Vertical");
+
+
+
+            if (input.x != 0) // No diagonal movement
+            {
+                input.y = 0;
+            }
+
 
             if (input != Vector2.zero)
             {
+                animator.SetFloat("moveX", input.x); // Give animator input
+                animator.SetFloat("moveY", input.y);
+                
                 var targetPos = transform.position;
                 targetPos.x += input.x;
                 targetPos.y += input.y;
@@ -27,7 +45,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    IEnumerator Move(Vector3 targetPos)
+    IEnumerator Move(Vector3 targetPos) // Character Movement
     {
         isMoving = true;
 
