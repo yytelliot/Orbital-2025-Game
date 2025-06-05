a0495d86ec9384583aac7cb8b045507d4c3ebb21
Elliot <yytelliot@gmail.com>
2025-06-05 18:30:41 +0800

    Simple Ammo System
    Simple UI and ShipProperties ammo implementation. Keeps track of how much ammo is left and prevents fire when ammo count is 0.

---
+++ Assets/Scripts/UI/AmmoCountScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoCountScript : MonoBehaviour
+{
+    public ShipProperties shipProperties;
+    public Text ammoText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ammoText.text = shipProperties.ammoCount.ToString();
+    }
+
+    public void UpdateUIAmmoCount()
+    {
+        ammoText.text = shipProperties.ammoCount.ToString();
+    }
+}
--- Assets/scripts/pilot/ShipProperties.cs
+++ Assets/scripts/pilot/ShipProperties.cs
@@ -5,4 +5,40 @@ using UnityEngine;
 public class ShipProperties : MonoBehaviour
 {
     public int health;
+    public int ammoCount;
+
+    public bool DeductAmmo()
+    {
+        if (ammoCount > 0)
+        {
+            ammoCount--;
+            return true;
+        }
+        else
+            return false;
+    }
+    
+    public bool DeductAmmo(int amount)
+    {
+        if (ammoCount >= amount)
+        {
+            ammoCount -= amount;
+            return true;
+        }
+        else
+            return false;
+
+    }
+
+    public bool DeductHealth(int amount)
+    {
+        if (health >= amount)
+        {
+            health -= amount;
+            return true;
+        }
+        else
+            return false;
+    }
+
 }
