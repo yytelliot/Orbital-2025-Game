36d3eda76f2846bcc7558b3ac2aff2ac7c23a2e8
Elliot <yytelliot@gmail.com>
2025-06-18 03:02:15 +0800

    Updated SendNetworkEvent and RPC_RaiseEvent
    Sending over a networked event now sends over the PhotonView component as the sender. Unfortunately this the closest thing to identifying the sender and will have to suffice for now.

--- Assets/Scripts/Network/CrossSceneNetworkManager.cs
+++ Assets/Scripts/Network/CrossSceneNetworkManager.cs
@@ -32,42 +32,60 @@ public class CrossSceneNetworkManager : MonoBehaviourPunCallbacks
         {
             Destroy(gameObject);
         }
-        
+
 
     }
+
+    // WARNING: unfortunately due to limitations with what you can send over via RPC, the sender
+    // of networked events will be filled as the component's PhotonView (or null if there isnt one)
     public void SendNetworkEvent(GameEvent gameEvent, Component sender, object data = null)
     {
-        int id = gameEvent.eventId;
+        int evtId = gameEvent.eventId;
+        PhotonView pv = sender.GetComponent<PhotonView>();
+
+        // if theres a photon view id send it, else default to 0 (null)
+        int senderViewId = pv != null
+            ? pv.ViewID
+            : 0;
 
         photonView.RPC(nameof(
             RPC_RaiseEvent),
             RpcTarget.Others,
-            id,
-            sender.GetInstanceID(),
+            evtId,
+            senderViewId,
             data);
     }
 
     [PunRPC]
-    private void RPC_RaiseEvent(int eventId, int senderId, object data)
+    private void RPC_RaiseEvent(int eventId, int senderViewId, object data)
     {
         GameEvent gameEvent = idToEvent[eventId];
-        Component sender = FindInstanceById(senderId);
+
+        // use PhotonView.Find to get the photonview component across the network
+        PhotonView senderPv = PhotonView.Find(senderViewId);
+
+        // send the photonview
+        Component sender = senderPv;
+            
         gameEvent.Raise(sender, data);
     }
 
-    private Component FindInstanceById(int id)
-    {
-        foreach (Component component in FindObjectsOfType<MonoBehaviour>())
-        {
-            if (id == component.GetInstanceID())
-            {
-                return component;
-            }
-        }
-        return null;
-    }
 
-    
+    // original function to find instance by component ID, no need for it now
+    /*
+        private Component FindInstanceById(int id)
+        {
+            foreach (Component component in FindObjectsOfType<MonoBehaviour>())
+            {
+                if (id == component.GetInstanceID())
+                {
+                    return component;
+                }
+            }
+            return null;
+        }
+    */
+
 
     /*public void SendTechnicianInteraction()
     {
