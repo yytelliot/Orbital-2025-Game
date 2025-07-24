3345300e8dcfecf1586aec0f233faae931a01741
Marnige <dragonmasteroffire@gmail.com>
2025-07-24 19:35:45 +0800

    Update ConnectToServer.cs

--- Assets/Scripts/Loading/ConnectToServer.cs
+++ Assets/Scripts/Loading/ConnectToServer.cs
@@ -6,9 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
+
+    private const string appId = "8c494d25-95c1-4a2a-a69d-fef185cade76";
+    private const string gameVersion = "1.0";
+    private const string fixedRegion = "asia";
     void Start()
     {
+        PhotonNetwork.PhotonServerSettings.AppSettings.AppIdRealtime = appId;
+        PhotonNetwork.PhotonServerSettings.AppSettings.AppVersion = gameVersion;
+        PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion = fixedRegion;
+        PhotonNetwork.GameVersion = gameVersion;
+
         PhotonNetwork.ConnectUsingSettings();
 
     }
